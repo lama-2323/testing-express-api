16fe58245283a4b2c8418ddbffbaa05330d68cd6
root <root@ubuntu.dev>
2018-03-06 00:29:17 +0200

    test trigger

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var app = express();
-var users = ['John', 'Betty', 'Hal'];
+var users = ['John', 'Betty', 'Hall'];
 
 app.get('/api/users', function (req, res) {
   res.json(users);
